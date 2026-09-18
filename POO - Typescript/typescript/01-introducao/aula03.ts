53b6e20ad0147827b4299673e81d6b9e50576119
EdsonRafaelS <eds.rrafael@gmail.com>
2026-09-18 18:48:29 -0300

    TypeScript - any uknown undefined null

---
+++ POO - Typescript/typescript/01-introducao/aula03.ts
@@ -0,0 +1,18 @@
+let valor : any;
+valor = 10;
+valor = "Rafael";
+valor = true;
+//Evite ultilizar ANY
+
+let valores : unknown;
+valores = 10;
+//Modo certo - Após adicionar ele verifica qual o tipo primitivo
+
+let value : string | undefined;
+value = "Rafael";
+value = undefined;
+//So não pode ser boolean ou number
+
+let vler : string | null = null;
+vler = "Rafael";
+vler = null;
